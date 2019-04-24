b6fd16df6f9e3316d400a9af429624fe632588ae
lukasb23 <lukas.boeck@gmx.at>
2019-04-24 15:45:56 +0200

    Update md_dialogflow.py

--- modules/md_dialogflow.py
+++ modules/md_dialogflow.py
@@ -1,5 +1,4 @@
 #Imports
-import os
 import json
 import dialogflow
 from google.api_core.exceptions import InvalidArgument
@@ -24,32 +23,30 @@ def detect_intent_texts(user_id, text):
     session_client = dialogflow.SessionsClient(credentials=credentials)
     session_dialogflow = session_client.session_path(project_id, user_id)
 
-    if text:
-        text_input = dialogflow.types.TextInput(
-            text=text, language_code=language_code
+    text_input = dialogflow.types.TextInput(
+        text=text, language_code=language_code
+    )
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(
+            session=session_dialogflow, query_input=query_input
         )
-        query_input = dialogflow.types.QueryInput(text=text_input)
-        try:
-            response = session_client.detect_intent(
-                session=session_dialogflow, query_input=query_input
-            )
-        except InvalidArgument:
-            return ("I'm sorry, but your message was too long for me to handle,\
-                    please stick to a maximum of 256 characters.", None, None)
-        
-        intent = response.query_result.intent.display_name
-        fields = response.query_result.parameters.fields
-                
-        #let Dialogflow handle all non-related queries
-        print('INTENT', intent)
-        if intent == "default-welcome-intent":
-            return (response.query_result.fulfillment_text,
-                    ["Guided search", "Custom search", "More info"], None)
-        
-        elif not intent.startswith("search"):
-            return (response.query_result.fulfillment_text, None, None)
-        
-        #custom logic
-        else:
-            return Search(text,user_id,intent,fields).logic()
-            
+    except InvalidArgument:
+        return ("I'm sorry, but your message was too long for me to handle,\
+                please stick to a maximum of 256 characters.", None, None)
+
+    intent = response.query_result.intent.display_name
+    fields = response.query_result.parameters.fields
+
+    #let Dialogflow handle all non-related queries
+    print('INTENT', intent)
+    if intent == "default-welcome-intent":
+        return (response.query_result.fulfillment_text,
+                ["Guided search", "Custom search", "More info"], None)
+
+    elif not intent.startswith("search"):
+        return (response.query_result.fulfillment_text, None, None)
+
+    #custom logic
+    else:
+        return Search(text,user_id,intent,fields).logic()
